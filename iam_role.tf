0f5abf7c992ea6399a23759bfe9ddaa558f2fd56
emre <emrehummet@hotmail.com>
2023-12-13 20:37:22 +0100

    added iam role and user-data

---
+++ iam_role.tf
@@ -0,0 +1,28 @@
+resource "aws_iam_role" "proje2_role" {
+  name = "proje2_backend-s3_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "example_attachment" {
+  role       = aws_iam_role.proje2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_instance_profile" "proje2_iam_profile" {
+  name = "role_profile"
+  role = aws_iam_role.proje2_role.name
+}
--- launch_template.tf
+++ launch_template.tf
@@ -1,15 +1,16 @@
 resource "aws_launch_template" "proje2_lt" {
   name                   = "template_instance"
-  image_id               = "ami-0230bd60aa48260c6"  # BU KISIM DAHA SONRA DATA BLOGU ILE HALLEDILIRSE IYI OLUR
+  image_id               = "ami-0bb5f71d17c1f8890"  # BU KISIM DAHA SONRA DATA BLOGU ILE HALLEDILIRSE IYI OLUR
   instance_type          = "t2.micro"
   key_name               = "firstkey"   #var.key_name             # KEYNAME VARIABLE'I OLUSTURULMALI
   vpc_security_group_ids = [aws_security_group.proje2_EC2_Sec_Group.id]
+  iam_instance_profile   = aws_iam_instance_profile.proje2_iam_profile.name
   user_data              = base64encode(data.template_file.proje2_tem_file.rendered)
 }
 
-# data "template_file" "proje2_tem_file" {
-#   template = file("user-data.sh")               # BU ISIMDE BIR USER DATA DOSYASI OLUSTURULMASI LAZIM
-# }
+data "template_file" "proje2_tem_file" {
+  template = file("user-data.sh")               
+}
 
 
 ########### LAUNCH TEMPLATE ICIN GEREKLI BILESENLER ###########
